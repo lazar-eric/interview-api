9ee354ea265c89cd5c5c93ed0b3d330e7b1850c8
Lazar Eric <lazareric.com@gmail.com>
2023-12-06 23:24:38 +0100

    Test setup

---
+++ test/routes/contracts.test.ts
@@ -0,0 +1,23 @@
+
+import { assert } from '@amaui/test';
+
+group('Contracts', () => {
+
+  to('asd', () => {
+    assert(true).truthy;
+  });
+
+  post(async () => {
+    // const uri = config.value.db.mysql.uri;
+
+    // const sequelize = new Sequelize(uri);
+
+    // // drop all the tables
+    // if (uri.includes('company-test')) {
+    //   await sequelize.sync();
+
+    //   await sequelize.drop();
+    // }
+  });
+
+});
